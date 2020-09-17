f7b1ff261142b6208327bcba4729fb79ecb78f8e
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-09-17 01:22:57 -0400

    finished some

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 //1.
-// const person = [{ name: "Jill" }, { name: "" }, { name: "Bob" }];
+ const person = [{ name: "Jill" }, { name: "" }, { name: "Bob" }];
 //create a function 'greeting' that maps through the array.
 // Use a ternary.
 // If there is a name it should say 'Hello <name>' otherwise it should say,
 // 'Hello Stranger'.
 
 
-
+let greet = function(arr){
+  arr.map(x => (x.name === '') ? console.log(`Hello Stranger`) : console.log(`Hello ${x.name}`))
+  }
+// greet(person)
 
 //2.
 ///Create a function that uses ternary to decide whether the person can drive
 // If they are below 15 then log 'cannot drive', if they are 15 then log 'can drive with a parent', if they are 16 or above then 'yes they can drive'
 
+const  drivers = [{ name: "Jill", age: 14}, { name: "Harry", age: 16 }, { name: "Bob" , age :15}]
 
+let canDrive = function (arr){
 
+  arr.map(x => x.age < 15 ? console.log(`${x.name} cannot drive`): (x.age === 15) ? console.log(`${x.name} can drive with a parent`) : console.log(`${x.name} can drive`))
+}
+
+// canDrive(drivers)
 
 //3.
 // create a variable speedMessage that has a value resulting from a ternary
@@ -26,7 +35,6 @@
 
 
 
-
 //4. 
 // map through an array and if the value is greater than 50 it goes in the arr1
 //if the array is less than 50 it goes in arr2
@@ -35,7 +43,8 @@
 //use ternary
 // use an array method BUT do not return a data structure when you iterate through your items.
 // OUTPUT:
-//[ 445, 4555 ][ 22, 33.99, 1, 44.98 ] [ 'peaches', 'apple' ]
+
+
 let arr = [
     'peaches',
     444.7,
@@ -54,11 +63,12 @@ let arr = [
   let arr2 = []
   let arr3 = []
   
+  arr.map(x => x > 50 ? arr1.push(x) : x < 15 ? arr2.push(x) : typeof x === "string" ? arr3.push(x) : arr2.push(x))
+  
+  // console.log(arr1, arr2, arr3)
   
   
-  
-  
-Object Methods:
+// Object Methods:
 5.//Use object methods to solve problem
 // Write a method that doubles the price of each item in the prices object and place those values in a new object called priceDoubler
 let prices = {
@@ -66,3 +76,5 @@ let prices = {
   coke: { lg: 2.99, sm: 1.99 },
   chips: 2.79,
 };
+
+
